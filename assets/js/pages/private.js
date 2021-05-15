7496e8c9cd602109841351bc47d27cdb7112ee4a
KNobHack <akudandia7272@gmail.com>
2021-05-15 12:29:52 +0700

    add private page

---
+++ assets/js/pages/private.js
@@ -0,0 +1,10 @@
+'use strict';
+$(document).ready(function () {
+    var px = new PerfectScrollbar('.pro-scroll', {
+        wheelSpeed: .5,
+        swipeEasing: 0,
+        wheelPropagation: 1,
+        minScrollbarLength: 40,
+    });
+    // [ campaign-scroll ] end
+});
